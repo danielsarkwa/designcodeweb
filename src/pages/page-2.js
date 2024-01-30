874088ab0f538c91a1eb740dd44997fead7be14c
DanielSarkwa <dhannysark@outlook.com>
2024-01-30 23:38:15 +0200

    addsd

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,7 @@ function SecondPage() {
     <Layout>
       <SEO title="Page two" />
       <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
+      <p>Welcome to page 2 - this is page 2</p>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   )
